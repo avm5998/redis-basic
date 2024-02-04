cfd1b8dc5c60ff43775999922ecf3eba8d41d6b6
Atharva Metkar <avm5998@gmail.com>
2024-02-03 23:33:25 -0500

    added a protocol for delivery

--- server.cpp
+++ server.cpp
@@ -7,11 +7,12 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <assert.h>
 
-static void msg(const char *msg) {
+static void msg(const char *msg)
+{
     fprintf(stderr, "%s\n", msg);
 }
-
 
 static void die(const char *msg)
 {
@@ -20,27 +21,92 @@ static void die(const char *msg)
     abort();
 }
 
-static void do_something(int connfd)
+static int32_t read_full(int fd, char *buf, size_t n)
 {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
+    while (n > 0)
+    {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1; // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1; // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd)
+{
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            msg("EOF");
+        }
+        else
+        {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err)
     {
         msg("read() error");
-        return;
+        return err;
     }
-    printf("client says: %s\n", rbuf);
 
-    char wbuf[] = "world";
-    int wbytes;
-    wbytes = write(connfd, wbuf, strlen(wbuf));
-    printf("Bytes written = %d", wbytes);
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main()
 {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(fd<0) {
+    if (fd < 0)
+    {
         die("socket()");
     }
     int val = 1;
@@ -67,7 +133,6 @@ int main()
     while (true)
     {
         // accept the request
-
         struct sockaddr_in client_addr = {};
         socklen_t socklen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
@@ -76,10 +141,15 @@ int main()
             continue;
         }
 
-        do_something(connfd);
+        while (true)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+            {
+                break;
+            }
+        }
         close(connfd);
     }
     return 0;
 }
-
-

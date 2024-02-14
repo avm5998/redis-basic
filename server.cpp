6ddbff2d06c56663ce26cb0e464ee5d443fac2b3
Atharva <avm5998@gmail.com>
2024-02-13 23:49:37 -0500

    added support for get,set,del cmds

--- server.cpp
+++ server.cpp
@@ -1,16 +1,18 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <poll.h>
+#include <string>
 #include <vector>
+#include <map>
 
 // maximum size of message in bytes
 const size_t k_max_msg = 4096;
@@ -122,6 +124,107 @@ static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd)
 static void state_req(Conn *conn);
 static void state_res(Conn *conn);
 
+static int32_t parse_req(const uint8_t *data,size_t len,std::vector<std::string>&out) {
+    if(len<4) {
+        return -1;
+    }
+    uint32_t n=0;
+    memcpy(&n,&data[0],4);
+    // n is the number of strings, nstr
+    if(n>k_max_msg) {
+        return -1;
+    }
+    size_t pos=4;
+    while(n--) {
+        if(pos+4 >len) {
+            // breaking condition coz this means there is no string here after its length is obtained
+            return -1;
+        }
+        uint32_t sz=0;
+        memcpy(&sz,&data[pos],4);
+        // index [pos,pos+3] stores the length of the string, which we store in sz
+        if(pos+4+sz>len &&n>0) {
+            return -1;
+        }
+        //sz is the length of the string
+        out.push_back(std::string((char *)&data[pos+4],sz));
+        pos+=4+sz;
+    }
+
+    if(pos!=len) {
+        return -1;
+        // our request has some extra input
+    }
+    return 0;
+}
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+
+static std::map<std::string,std::string> g_map;
+
+static uint32_t do_get(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen) {
+    if(!g_map.count(cmd[1])) {
+        return RES_NX;
+    }
+    std::string &val= g_map[cmd[1]];
+    assert(val.size()<=k_max_msg);
+    // we use val.data() since using memcpy on val, a string is not safe.
+    // we copy the value into the response
+    memcpy(res,val.data(),val.size());
+    *reslen=(uint32_t)val.size();
+    // we return a status code that says ok
+    return RES_OK;
+}
+static uint32_t do_set(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen) {
+    (void)res;
+    (void)reslen;
+    g_map[cmd[1]]=cmd[2];
+    return RES_OK;
+}
+static uint32_t do_del(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen) {
+    // used to explicitly indicate to the compiler that the variables res and reslen are intentionally not used within the function. 
+    // This technique is often employed to suppress compiler warnings about unused variables.
+    (void)res;
+    (void)reslen;
+    g_map.erase(cmd[1]);
+    return RES_OK;
+}
+
+// const ensures that the function doesnt modify the parameters passed to it
+static bool cmd_is(const std::string &word,const char *cmd) {
+    return 0 == strcasecmp(word.c_str(),cmd);
+}
+
+
+static int32_t do_request(const uint8_t *req,uint32_t reqlen,uint32_t *rescode,uint8_t *res,uint32_t *reslen) {
+    std::vector<std::string>cmd;
+    if(0!=parse_req(req,reqlen,cmd)) {
+        msg("bad req");
+        return -1;
+    }
+
+    if(cmd.size()==2&& cmd_is(cmd[0],"get")) {
+        *rescode=do_get(cmd,res,reslen);
+    } else if(cmd.size()==2&&cmd_is(cmd[0],"del")) {
+        *rescode=do_del(cmd,res,reslen);
+    } else if(cmd.size()==3&&cmd_is(cmd[0],"set")) {
+        *rescode=do_set(cmd,res,reslen);
+    } else {
+        // cmd is not recognized
+        *rescode=RES_ERR;
+        const char *msg="Unknown cmd";
+        strcpy((char *)res,msg);
+        *reslen=strlen(msg);
+    }
+    return 0;
+
+}
+
 static bool try_one_request(Conn *conn)
 {
     // try and parse a request from the buffer
@@ -132,6 +235,8 @@ static bool try_one_request(Conn *conn)
     }
     uint32_t len = 0;
     memcpy(&len, &conn->rbuf[0], 4);
+    //this len here is the number of strings in the command
+
     // 4 coz we need 4 bytes, which store the length of the message in them
     if (len > k_max_msg)
     {
@@ -148,29 +253,37 @@ static bool try_one_request(Conn *conn)
         return false;
     }
 
-    // got one request, print it
-    printf("client says: %.*s\n", len, &conn->rbuf[4]);
+    uint32_t rescode = 0;
+    uint32_t wlen=0;
+    int32_t err=do_request(&conn->rbuf[4],len,&rescode, &conn->wbuf[4+4],&wlen);
 
-    // generate the response for this request
-    memcpy(&conn->wbuf[0], &len, 4);
-    memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
-    conn->wbuf_size = 4 + len;
-
-    // remove the request from the buffer
-    //  note: frequent memmove is inefficient.
-    //  note: need better handling for production code.
-    size_t remain = conn->rbuf_size - 4 - len;
-    if (remain)
-    {
-        memmove(conn->rbuf, &conn->rbuf[4 + len], remain);
+    if(err) {
+        conn->state=STATE_END;
+        return false;
     }
-    conn->rbuf_size = remain;
+    wlen+=4;
+    // first 4 bytes of wbuf are for the length of the succeeding message- both rescode and data
+    memcpy(&conn->wbuf[0],&wlen,4);
+    // next 4 bytes are for the rescode
+    memcpy(&conn->wbuf[4],&rescode,4);
+    conn->wbuf_size=4+wlen;
 
-    // change state
-    conn->state = STATE_RES;
+    // remove the request from the buffer.
+    // note: frequent memmove is inefficient.
+    // note: need better handling for production code.
+
+    size_t remain=conn->rbuf_size - len -4;
+    if(remain) {
+        memmove(conn->rbuf,&conn->rbuf[len+4],remain);
+    }
+    conn->rbuf_size=remain;
+
+    //change state
+    conn->state=STATE_RES;
     state_res(conn);
 
-    return (conn->state == STATE_REQ);
+    return (conn->state==STATE_REQ);
+
 }
 
 static bool try_fill_buffer(Conn *conn)
